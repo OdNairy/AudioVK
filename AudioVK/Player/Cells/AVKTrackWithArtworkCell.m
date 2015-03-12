468027b3db23e2d5bbabb284900faffecdb8bc96
Roman Gardukevich <roman.gardukevich@intellectsoft.by>
2015-03-12 19:20:31 +0300

    Added Autoscroll Label

--- AudioVK/Player/Cells/AVKTrackWithArtworkCell.m
+++ AudioVK/Player/Cells/AVKTrackWithArtworkCell.m
@@ -7,11 +7,13 @@
 //
 
 #import "AVKTrackWithArtworkCell.h"
+#import <MarqueeLabel.h>
 
 @interface AVKTrackWithArtworkCell ()
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *titleToDownloadButtonConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *titleTrailingConstraint;
 @property (nonatomic, weak) IBOutlet UIView* downloadView;
+@property (weak, nonatomic) IBOutlet MarqueeLabel *titleMarqueeLabel;
 @end
 
 @implementation AVKTrackWithArtworkCell
@@ -19,6 +21,14 @@
 - (void)awakeFromNib {
     // Initialization code
     self.titleLabel.preferredMaxLayoutWidth = [UIScreen mainScreen].bounds.size.width;
+    self.titleMarqueeLabel.scrollDuration = 30;
+    self.titleMarqueeLabel.marqueeType = MLLeftRight;
+//    self.titleMarqueeLabel.scrollDuration = 15.0;
+    self.titleMarqueeLabel.animationCurve = UIViewAnimationOptionCurveEaseInOut;
+    self.titleMarqueeLabel.fadeLength = 10.0f;
+    self.titleMarqueeLabel.leadingBuffer = 0;
+    self.titleMarqueeLabel.trailingBuffer = 0;
+    self.titleMarqueeLabel.tag = 101;
 }
 
 -(void)layoutSubviews{
